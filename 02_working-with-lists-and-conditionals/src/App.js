4f3493bdb0167310cb542b0e23eefec21ce1a8a1
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-12-18 20:51:32 +0100

    adds flexible-lists

--- 02_working-with-lists-and-conditionals/src/App.js
+++ 02_working-with-lists-and-conditionals/src/App.js
@@ -11,15 +11,24 @@ class App extends Component {
     ]
   }
 
-  nameChangedHandler = (event) => {
-      this.setState({
-          persons: [
-              { name: 'Tajib', age: 24 },
-              { name: event.target.value, age: 17 },
-              { name: 'Kemal', age: 56 }
-          ]
-      } )
-  }
+    nameChangedHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState( {persons: persons} );
+    }
 
   deletePersonHandler = (personIndex) => {
       // const persons = this.state.persons.slice()
@@ -47,7 +56,7 @@ class App extends Component {
         if ( this.state.showPersons ) {
             persons = (
                 <div>
-                    {this.state.persons.map((person, index) => <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id}/>)}
+                    {this.state.persons.map((person, index) => <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} change={(event) => this.nameChangedHandler(event, person.id)}/>)}
                 </div>
             )
         }
