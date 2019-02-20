c6bc23dbb59b2f59d0cc2a014037f615698f4bae
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-02-20 14:21:50 +0100

    improving performance of the App, adds component lifecycle method and changing folder structure

--- 06_burger-builder-(basic-version)/src/components/Navigation/SideDrawer/SideDrawer.js
+++ 06_burger-builder-(basic-version)/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,26 @@ import React from 'react'
 import classes from './SideDrawer.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 
 const sideDrawer = (props) => {
-
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
 
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height='11%' marginBottom='32px'/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join( ' ')}>
+                <Logo height='11%' marginBottom='32px'/>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
+
     )
 }
 
