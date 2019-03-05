a703190b1c072be9c7c5a877a8e27b0dd736fbae
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-03-05 18:38:56 +0100

    adds extracting Route parameters, using Switch to load a single Route, navigating programmatically and nested Routes

--- 09_routing/src/containers/Blog/Blog.js
+++ 09_routing/src/containers/Blog/Blog.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 // import axios from '../../axios';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
     state = {
@@ -22,13 +21,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to='/'
+                                to='/posts/'
                                 exact
                                 activeClassName='my-active'
                                 activeStyle={{
                                    color: 'violet',
                                    textDecoration: 'underline'
-                                }}>Home</NavLink></li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname:'/new-post',
                                 hash: '#submit',
@@ -39,9 +38,11 @@ class Blog extends Component {
                 </header>
                 {/*<Route path='/' exact render={() => <Posts/>}/>
                 <Route path='/new-post' exact render={() => <h1 style={{textAlign: 'center'}}>New Post</h1>}/>*/}
-                <Route path='/' exact component={Posts}/>
-                <Route path='/new-post' component={NewPost}/>
-                <Route path='/:id' component={FullPost}/>
+
+                <Switch>
+                    <Route path='/new-post' component={NewPost}/>
+                    <Route path='/posts' component={Posts}/>
+                </Switch>
             </div>
         );
     }
--- 09_routing/src/containers/Blog/Posts/Posts.js
+++ 09_routing/src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import axios from '../../../axios'
-import { Link } from 'react-router-dom'
+import {Route} from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import Post from '../../../components/Post/Post'
 import'./Posts.css'
+import FullPost from '../FullPost/FullPost'
 
 class Posts extends Component {
 	state = {
@@ -11,6 +13,7 @@ class Posts extends Component {
     }
 
 	componentDidMount () {
+		console.log(this.props)
        axios.get( '/posts' )
            .then( response => {
                const posts = response.data.slice(0, 4);
@@ -29,8 +32,9 @@ class Posts extends Component {
            });
    }
 
-	postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+	postSelectedHandler = ( id ) => {
+        // this.props.history.push({pathname: '/posts/' + id});
+        this.props.history.push( '/posts/' + id );
     }
 
 	render () {
@@ -38,20 +42,24 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                	<Link to={'/' + post.id} key={post.id}>
+                	// <Link to={'/' + post.id} key={post.id}>
 	                    <Post
+		                key={post.id}
 	                    title={post.title}
 	                    body={post.body}
 	                    author={post.author}
-	                    clicked={() => this.postSelectedHandler(post.id)} />
-	                </Link>)
+	                    clicked={() => this.postSelectedHandler(post.id)} />)
+	                // </Link>)
             });
         }
 
 		return (
-			<section className="Posts">
-				{posts}
-			</section>
+			<div>
+				<section className="Posts">
+					{posts}
+				</section>
+				<Route path={this.props.match.url + '/:id'} exact component={FullPost}/>
+			</div>
 		)
 	}
 }
