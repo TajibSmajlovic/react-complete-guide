575f89483fcc43ce5a4d5910fa7419bb7e03f68b
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-04-10 10:49:50 +0200

    connecting React to Redux, Store to React and dispatching Actions from within the Component

--- 12_redux/redux-basics.js
+++ 12_redux/redux-basics.js
@@ -1,36 +1,37 @@
-const redux = require('redux')
-const createStore = redux.createStore
+const redux = require('redux');
+const createStore = redux.createStore;
 
 const initialState = {
-	counter: 0
+    counter: 0
 }
 
-//Reducer
+// Reducer
 const rootReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'INC_COUNTER':
-			return {
-				...state,
-				counter: state.counter + 1
-			}
-		case 'ADD_COUNTER':
-			return {
-				...state,
-				counter: state.counter + action.value
-			}
-		default: return state
-}}
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+    return state;
+};
 
-//Store
-const store = createStore(rootReducer)
-console.log(store.getState())
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 // Subscription
 store.subscribe(() => {
-	console.log('[Subscription]', store.getState())
-})
+    console.log('[Subscription]', store.getState());
+});
 
-// Dispatch Action
-store.dispatch({type: 'INC_COUNTER'})
-store.dispatch({type: 'ADD_COUNTER', value: 10})
-console.log(store.getState())
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
